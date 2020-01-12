ee9a83fb17f330cd3bffe8d6316871aef85e9192
jbernste <ankbitinfo@gmail.com>
2020-01-11 19:33:53 -0800

    added build

---
+++ client/build/precache-manifest.2ea9aefdbe6ee445a9ea232f017504c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6739d49a01a3d695769ad46e733c01d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7169d1226c6192cdc02f",
+    "url": "/static/css/main.6212d54d.chunk.css"
+  },
+  {
+    "revision": "99d361cc84fb055a09bd",
+    "url": "/static/js/2.de39f93f.chunk.js"
+  },
+  {
+    "revision": "5ec987632bc308bcd1222d7283dbdad9",
+    "url": "/static/js/2.de39f93f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7169d1226c6192cdc02f",
+    "url": "/static/js/main.3bcf8b1c.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "5b81caf9b9f7442ab4c36f5b7eef5c7d",
+    "url": "/static/media/spinner.5b81caf9.gif"
+  }
+]);
